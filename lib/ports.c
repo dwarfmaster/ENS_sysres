82d0eaf9fabecc35a2c25da5467f10eeadd12f65
lucas8 <luc.linux@mailoo.org>
2017-04-14 20:41:05 +0200

    Add ports helper library

---
+++ lib/ports.c
@@ -0,0 +1,52 @@
+
+#include "ports.h"
+#include <string.h>
+
+struct message_full_header {
+    mach_msg_header_t head;
+    mach_msg_type_t type;
+};
+
+int send_data(mach_port_t port, const typeinfo_t* info, char* data) {
+    mach_msg_return_t err;
+    struct message_full_header* hd = (struct message_full_header*)data;
+    unsigned int size = info->size * info->number;
+    memmove(data + sizeof(struct message_full_header), data, size);
+
+    hd->head.msgh_bits = MACH_MSGH_BITS_REMOTE(
+            MACH_MSG_TYPE_MAKE_SEND);
+    hd->head.msgh_size = size + sizeof(struct message_full_header);
+    hd->head.msgh_local_port = MACH_PORT_NULL;
+    hd->head.msgh_remote_port = port;
+
+    hd->type.msgt_name = info->id;
+    hd->type.msgt_size = info->size;
+    hd->type.msgt_number = info->number;
+    hd->type.msgt_inline = TRUE;
+    hd->type.msgt_longform = FALSE;
+    hd->type.msgt_deallocate = FALSE;
+
+    err = mach_msg( &hd->head, MACH_SEND_MSG,
+            hd->head.msgh_size, 0, MACH_PORT_NULL,
+            MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+    if(err != MACH_MSG_SUCCESS) return 0;
+    return 1;
+}
+
+/* TODO use timeout */
+int receive_data(mach_port_t port, typeinfo_t* info, char* buffer, size_t size) {
+    mach_msg_return_t err;
+    struct message_full_header* hd = (struct message_full_header*)buffer;
+    hd->head.msgh_size = size;
+    err = mach_msg( &hd->head, MACH_RCV_MSG,
+            0, hd->head.msgh_size, port,
+            MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+    if(err != MACH_MSG_SUCCESS) return 0;
+
+    info->id     = hd->type.msgt_name;
+    info->size   = hd->type.msgt_size;
+    info->number = hd->type.msgt_number;
+    memmove(buffer, buffer + sizeof(struct message_full_header), info->size * info->number);
+    return 1;
+}
+
---
+++ lib/ports.h
@@ -0,0 +1,29 @@
+
+#ifndef DEF_LIB_PORTS
+#define DEF_LIB_PORTS
+
+#include <mach.h>
+#include <stdlib.h>
+
+/* Info about the type in the message. id can be arbitrarily
+ * set : it isn't used during the transaction, only by the
+ * receiver.
+ * size and number are used to determine the size of the data
+ * to send when sending.
+ */
+typedef struct typeinfo {
+    unsigned int id;
+    unsigned int size;
+    unsigned int number; /* Number of elements of that type */
+} typeinfo_t;
+
+/* Assumes there is space after data, ie it's in a buffer bigger than
+ * info->size * info->number by at least 64 bytes
+ * data may be ovewritten during the sending.
+ */
+int send_data(mach_port_t port, const typeinfo_t* info, char* data);
+/* info is only set, not read here */
+int receive_data(mach_port_t port, typeinfo_t* info, char* buffer, size_t size);
+
+#endif//DEF_LIB_PORTS
+
